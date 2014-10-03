fa81b2f3e5e24ea414bb07e80a0777a137ce9780
Corey Berman <coreymberman@gmail.com>
2014-10-02 20:15:47 -0500

    first commit

---
+++ routes.js
@@ -0,0 +1,62 @@
+Router.configure({
+  layoutTemplate: 'layout',
+  yieldTemplates: {
+    'header': {to: 'header'},
+    'footer': {to: 'footer'}
+  },
+  loadingTemplate: 'loading',
+  notFoundTemplate: '404',
+  waitOn: function (){
+    return [
+    Meteor.subscribe("Users")
+    ];
+  },
+  data: function(){
+
+  },
+  action: function () {
+
+  }
+});
+
+/* https://github.com/EventedMind/iron-router/issues/554 */
+Router.onBeforeAction('loading');
+
+Router.map(function () {
+  this.route('home', {
+    path: '/',
+    onBeforeAction: function(){
+      Session.set('routeName', 'Home');
+      $('body').scrollTop(0);
+    }
+  });
+
+  this.route('news', {
+    path: '/news',
+  });
+
+  this.route('project', {
+    path: 'project/:_projectName?',
+    template: 'project',
+    data: function(){
+      Session.set('projectName', this.params._projectName);
+      return {
+        projectName: this.params._projectName
+      }
+    },
+    onBeforeAction: function(){
+      Session.set('routeName', 'Projects');
+      $('body').scrollTop(0);
+    }
+  });
+
+
+  this.route('admin', {
+    path: '/admin',
+    onBeforeAction: function() {
+      if (!AuthManager.userIsInRole(Meteor.userId(), ['admin'])) {
+        Router.go("/");
+      }
+    }
+  });
+});
---
+++ server/publish.js
@@ -0,0 +1,40 @@
+/* publish.js
+Publishes collections
+Sets permissions
+*/
+
+Meteor.publish("Users", function(){
+	return Meteor.users.find();
+});
+
+
+Meteor.publish("Projects", function(){
+	return Projects.find();
+});
+
+Projects.allow({
+	insert: function(userId, doc){
+		var user = Meteor.users.find(userId);
+		if( AuthManager.userIsInRole(userId, ['admin']) ){
+			return true;
+		}else{
+			return true; //false
+		}
+	},
+	update: function(userId, doc){
+		var user = Meteor.users.find(userId);
+		if( AuthManager.userIsInRole(userId, ['admin']) ){
+			return true;
+		}else{
+			return false;
+		}
+	},
+	remove: function(userId, doc){
+		var user = Meteor.users.find(userId);
+		if( AuthManager.userIsInRole(userId, ['admin']) ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+});
